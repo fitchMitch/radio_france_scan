30d255536091bddf85f3544a131283a6797ac1b6
Etienne WEIL <weil.etienne@hotmail.fr>
2019-03-15 16:48:11 +0100

    initial commit

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source "https://rubygems.org"
 gem "rails", "~> 5.0.7"
 gem "graphql", "1.2.2"
 gem "graphql-client", "0.2.3"
+
+gem "better_errors"
+gem "binding_of_caller"
--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module GitHub
 
   HTTPAdapter = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
     def headers(context)
-      unless token = context[:access_token] || Application.secrets.github_access_token
+      token = context[:access_token] || Application.secrets.github_access_token
+      if token.nil?
         # $ GITHUB_ACCESS_TOKEN=abc123 bin/rails server
         #   https://help.github.com/articles/creating-an-access-token-for-command-line-use
         fail "Missing GitHub access token"
